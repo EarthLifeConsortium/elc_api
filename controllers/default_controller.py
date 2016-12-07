73b6ced13400d98c03853d70868aaedfaa60cfb7
jpjenk <jpjenk@icloud.com>
2016-12-07 15:00:06 -0500

    Added the /site endpoint. Changed publication endpoint to /pub to be consistent.

--- controllers/default_controller.py
+++ controllers/default_controller.py
@@ -1,5 +1,8 @@
 
-def pubs(occId = None, siteId = None, format = None) -> str:
+def pub(occId = None, siteId = None, format = None) -> str:
+    return 'do some magic!'
+
+def site(occId = None, bbox = None, minAge = None, maxAge = None, ageScale = None, timeRule = None, taxon = None, includeLower = None) -> str:
     return 'do some magic!'
 
 def taxon(taxon = None, includeLower = None, hierarchy = None) -> str:
