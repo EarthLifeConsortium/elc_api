e3f1eb86274aa115a518f10eb2c68286fb75c481
jpjenk <jpjenk@icloud.com>
2016-12-07 10:53:49 -0500

    Added the /taxon endpoint

---
+++ controllers/default_controller.py
@@ -0,0 +1,6 @@
+
+def pubs(occId = None, siteId = None, format = None) -> str:
+    return 'do some magic!'
+
+def taxon(taxon = None, includeLower = None, hierarchy = None) -> str:
+    return 'do some magic!'
