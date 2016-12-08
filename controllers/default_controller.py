64130c7fec768d4d17a2679d7b99f728be8bb089
jpjenk <jpjenk@icloud.com>
2016-12-07 20:10:33 -0500

    Added the /occ endpoint. Cleaned up other endpoint definitions.

--- controllers/default_controller.py
+++ controllers/default_controller.py
@@ -2,6 +2,9 @@
 def age(occId = None, siteId = None) -> str:
     return 'do some magic!'
 
+def occ(bbox = None, minAge = None, maxAge = None, ageScale = None, timeRule = None, taxon = None, includeLower = None, limit = None, offset = None) -> str:
+    return 'do some magic!'
+
 def pub(occId = None, siteId = None, format = None) -> str:
     return 'do some magic!'
 
