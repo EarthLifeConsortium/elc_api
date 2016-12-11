ed3fcea93c8a9d4bb1cf6285d0e689c31677855d
jpjenk <jpjenk@icloud.com>
2016-12-10 19:34:26 -0500

    default_controller.py: Added more descriptive testing returns for endpoint stubs.

--- controllers/default_controller.py
+++ controllers/default_controller.py
@@ -1,18 +1,18 @@
 
 def age(occId = None, siteId = None) -> str:
-    return 'do some magic!'
+    return 'age basis endpoint'
 
 def grid(ageBound = None, ageBin = None, ageUnit = None, bbox = None, spatialBin = None, varUnit = None, presence = None) -> str:
-    return 'do some magic!'
+    return 'gridded assemblage endpoint'
 
 def occ(bbox = None, minAge = None, maxAge = None, ageScale = None, timeRule = None, taxon = None, includeLower = None, limit = None, offset = None) -> str:
-    return 'do some magic!'
+    return 'occurrence endpoint'
 
 def pub(occId = None, siteId = None, format = None) -> str:
-    return 'do some magic!'
+    return 'publication endpoint'
 
 def site(occId = None, bbox = None, minAge = None, maxAge = None, ageScale = None, timeRule = None, taxon = None, includeLower = None) -> str:
-    return 'do some magic!'
+    return 'site endpoint'
 
 def taxon(taxon = None, includeLower = None, hierarchy = None) -> str:
-    return 'do some magic!'
+    return 'taxonomy endpoint'
