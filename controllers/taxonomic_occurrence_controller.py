00e7e768cae8662291a6eaf6802730c2525f8182
jpjenk <jpjenk@icloud.com>
2017-01-26 22:27:45 -0500

    DB subquery success checks.

--- controllers/taxonomic_occurrence_controller.py
+++ controllers/taxonomic_occurrence_controller.py
@@ -1,5 +1,4 @@
-"""
-RESTful API controller.
+"""RESTful API controller.
 
 Endpoint for queries on taxonomic occurrences in time and space.
 """
@@ -8,14 +7,15 @@ from flask import request, jsonify
 import requests
 import time
 
-def occ(bbox = None, minage = None, maxage = None, agescale = None, timerule = None,
-        taxon = None, includelower = None, limit = None, offset = None) -> str:
+
+def occ(bbox=None, minage=None, maxage=None, agescale=None, timerule=None,
+        taxon=None, includelower=None, limit=None, offset=None) -> str:
+    """Return occurrence identifiers from Neotoma and PBDB."""
     occ_return = list()
     desc_obj = dict()
 
-    #
     # Query the Neotoma Database (Occurrences)
-    #
+
     t0 = time.time()
     neotoma_base = 'http://apidev.neotomadb.org/v1/data/occurrences'
     payload = dict()
@@ -37,9 +37,11 @@ def occ(bbox = None, minage = None, maxage = None, agescale = None, timerule = N
 
     if 'includelower' in request.args:
         if request.args.get('includelower').lower() == 'false':
-            payload.update(nametype='tax', taxonname=request.args.get('taxon'))
+            payload.update(nametype='tax',
+                           taxonname=request.args.get('taxon'))
         else:
-            payload.update(nametype='base', taxonname=request.args.get('taxon'))
+            payload.update(nametype='base',
+                           taxonname=request.args.get('taxon'))
     else:
         payload.update(nametype='base', taxonname=request.args.get('taxon'))
 
@@ -54,24 +56,23 @@ def occ(bbox = None, minage = None, maxage = None, agescale = None, timerule = N
     neotoma_res = requests.get(neotoma_base, params=payload, timeout=None)
 
     if neotoma_res.status_code == 200:
-        occ_cnt = 0
         neotoma_json = neotoma_res.json()
-        for occ in neotoma_json['data']:
-            occ_cnt += 1
-            db_occ_id = 'neot:occ:' + str(occ['OccurID'])
-            occ_return.append({'occ_id': db_occ_id, 'taxon': occ['TaxonName']})
-        t1 = round(time.time() - t0, 5)
-        desc_obj.update(neot_time=t1)
-        desc_obj.update(neot_url=neotoma_res.url)
-        desc_obj.update(neot_occs=occ_cnt)
+        if 'data' in neotoma_json:
+            for occ in neotoma_json['data']:
+                db_occ_id = 'neot:occ:' + str(occ['OccurID'])
+                occ_return.append({'occ_id': db_occ_id,
+                                   'taxon': occ['TaxonName']})
+            t1 = round(time.time() - t0, 5)
+            desc_obj.update(neot_time=t1)
+            desc_obj.update(neot_url=neotoma_res.url)
+            desc_obj.update(neot_occs=len(neotoma_json['data']))
 
-    #
     # Query the Paleobiology Database (Occurrences)
-    #
+
     t0 = time.time()
     pbdb_base = 'http://paleobiodb.org/data1.2/occs/list.json'
     payload = dict()
-    #payload.update(show='loc,coords,coll')
+    # payload.update(show='loc,coords,coll')
     payload.update(vocab='pbdb')
 
     if 'bbox' in request.args:
@@ -108,16 +109,17 @@ def occ(bbox = None, minage = None, maxage = None, agescale = None, timerule = N
     pbdb_res = requests.get(pbdb_base, params=payload, timeout=None)
 
     if pbdb_res.status_code == 200:
-        occ_cnt = 0
         pbdb_json = pbdb_res.json()
+        if 'records' in pbdb_json:
         for occ in pbdb_json['records']:
-            occ_cnt += 1
             db_occ_id = 'pbdb:occ:' + str(occ['occurrence_no'])
-            occ_return.append({'occ_id': db_occ_id, 'taxon': occ['identified_name']})
-        t1 = round(time.time() - t0, 5)
-        desc_obj.update(pbdb_time=t1)
-        desc_obj.update(pbdb_url=pbdb_res.url)
-        desc_obj.update(pbdb_occs=occ_cnt)
-        return jsonify(description=desc_obj, data=occ_return)
-    else:
-        return 'Error code ' + str(pbdb_res.status_code)
+            occ_return.append({'occ_id': db_occ_id,
+                               'taxon': occ['identified_name']})
+            t1 = round(time.time() - t0, 5)
+            desc_obj.update(pbdb_time=t1)
+            desc_obj.update(pbdb_url=pbdb_res.url)
+            desc_obj.update(pbdb_occs=len(pbdb_json['records']))
+
+    # Composite response 
+
+    return jsonify(description=desc_obj, data=occ_return)
