9789c9e2cdaae9c306cb3247010b1aba6528f2ce
jpjenk <jpjenk@icloud.com>
2017-04-11 22:03:06 -0400

    Continuation of site controller development.

--- swagger_server/controllers/site_controller.py
+++ swagger_server/controllers/site_controller.py
@@ -20,6 +20,9 @@ from statistics import mean
 def site(occid=None, bbox=None, minage=None, maxage=None, agescale=None, timerule=None, taxon=None, includelower=None):
     """
     Return site identifiers from Neotoma and PBDB.
+
+    For this API, a site is considered to be a geolocated group of 
+    Neotoma datasets and PBDB collections.
     """
     # Initialization and parameter checks
 
@@ -40,6 +43,8 @@ def site(occid=None, bbox=None, minage=None, maxage=None, agescale=None, timerul
     t0 = time.time()
     neotoma_base = 'http://apidev.neotomadb.org/v1/data/datasets'
     payload = dict()
+
+    ### if occid:
 
     if bbox:
         payload.update(bbox=bbox)
@@ -77,7 +82,73 @@ def site(occid=None, bbox=None, minage=None, maxage=None, agescale=None, timerul
         payload.update(nametype='base', taxonname=taxon)
 
     neotoma_res = requests.get(neotoma_base, params=payload, timeout=None)
+    
+    ### Parse neotoma response
+
+    # Query the Paleobiology Database (Sites)
+
+    t0 = time.time()
+    pbdb_base = 'http://paleobiodb.org/data1.2/colls/list.json'
+    payload = dict()
+    payload.update(show='loc,coords,coll')
+    payload.update(vocab='pbdb')
+
+    if occid:
+        payload.update(occ_id=occid)
+
+    if bbox:
+        bbox_list = bbox.split(',')
+        payload.update(lngmin=bbox_list[0], latmin=bbox_list[1],
+                       lngmax=bbox_list[2], latmax=bbox_list[3])
+
+    if agescale and agescale.lower() == 'yr':
+        age_scaler = 1e06
+        age_units = 'yr'
+        if minage:
+            payload.update(min_ma=float(minage)/age_scaler)
+        if maxage:
+            payload.update(max_ma=float(maxage)/age_scaler)
+    elif agescale and agescale.lower() == 'ka':
+        age_scaler = 1e03
+        age_units = 'ka'
+        if minage:
+            payload.update(min_ma=float(minage)/age_scaler)
+        if maxage:
+            payload.update(max_ma=float(maxage)/age_scaler)
+    else:
+        age_scaler = 1
+        age_units = 'ma'
+        if minage:
+            payload.update(min_ma=minage)
+        if maxage:
+            payload.update(max_ma=maxage)
+
+    if timerule:
+        payload.update(timerule=timerule)
+
+    if includelower and includelower.lower() == 'false':
+        payload.update(taxon_name=taxon)
+    else:
+        payload.update(base_name=taxon)
+
+    pbdb_res = requests.get(pbdb_base, params=payload, timeout=None)
+
+    if pbdb_res.status_code == 200:
+        pbdb_json = pbdb_res.json()
+        if 'records' in pbdb_json:
+            for site in pbdb_json['records']:
+                site_obj = dict()
+                site_id = 'pbdb:sit:' + str(site['collection_no'])
+                site_obj.update(site_id=site_id,
+                                site_name=site['collection_name'],
+                                lat=site['lat'],
+                                lon=site['lng'])
+
+            t1 = round(time.time()-t0, 5)
+            desc_obj.update(pbdb_time=t1)
+            desc_obj.update(pbdb_url=pbdb_res.url)
+            desc_obj.update(pbdb_occs=len(pbdb_json['records']))
 
     # Composite response
 
-    return jsonify(description=desc_obj, data=occ_return)
+    return jsonify(description=desc_obj, records=occ_return)
