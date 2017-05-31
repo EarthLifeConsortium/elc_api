1fa713a6835e6d0d15956ec1984932deb0707d62
jpjenk <jpjenk@icloud.com>
2017-05-31 10:19:43 -0400

    Publication endpoint additions:     - Enabled Locale ID search (datasetid for neotoma, coll_id for pbdb)     - No queries Neotoma     - RegEx extraction of doi from neotoma citation string

--- swagger_server/controllers/publication_controller.py
+++ swagger_server/controllers/publication_controller.py
@@ -15,20 +15,20 @@ def format_bibjson(ref):
     bib = dict()
 
     # Add basic reference info
-    bib.update(type=ref['kind'], year=ref['year'],
-               title=ref['title'], citation=ref['cite'])
+    bib.update(type=ref.get('kind'), year=ref.get('year'),
+               title=ref.get('title'), citation=ref.get('cite'))
 
     # Add info specific to the journal or book
-    bib.update(journal=[{'name': ref['journal'], 'volume': ref['vol'],
-                         'pages': ref['pages'], 'editors': ref['editor']}])
+    bib.update(journal=[{'name': ref.get('journal'), 'volume': ref.get('vol'),
+                         'pages': ref.get('pages'), 'editors': ref.get('editor')}])
 
     # Add reference locator IDs
-    bib.update(identifier=[{'type': 'doi', 'id': ref['doi']},
-                           {'type': 'db_index', 'id': ref['ident']}])
+    bib.update(identifier=[{'type': 'doi', 'id': ref.get('doi')},
+                           {'type': 'db_index', 'id': ref.get('ident')}])
 
     # Sequentially add authors to the authors block
     bib.update(author=[])
-    for author in ref['authors']:
+    for author in ref.get('authors'):
         bib['author'].append({'name': author})
 
     # End subroutine: format_bibjson
@@ -49,14 +49,14 @@ def format_ris(ref):
     return bib
 
 
-def pub(occid=None, siteid=None, format=None):
+def pub(occid=None, locid=None, format=None):
     """Return primary reference data in BibJSON, CSV or RIS format."""
     # Initialization and parameter checks
     desc_obj = dict()
     pub_return = list()
     if request.args == {}:
         return jsonify(status_code=400, error='No parameters provided.')
-    if occid and siteid:
+    if occid and locid:
         return jsonify(status_code=400,
                        error='Specify only occurrence ID or site ID not both.')
 
@@ -64,9 +64,74 @@ def pub(occid=None, siteid=None, format=None):
     # Query the Neotoma Database (Publications)
     #
 
+    t0 = time.time()
+    payload = dict()
+
+    # Select the appropriate URI for occs or sites/colls
+    if occid:
+        base_url = ''
+        payload.update(occ_id='')
+    elif locid:
+        base_url = 'http://api.neotomadb.org/v1/data/publications'
+        payload.update(datasetid=locid)
+    else:
+        return jsonify(status_code=400,
+                       error='Specify occurrence ID or locale ID.')
+
+    # Issue GET request to database API
+    resp = requests.get(base_url, params=payload, timeout=5)
+
+    if resp.status_code == 200:
+        resp_json = resp.json()
+        if 'data' in resp_json:
+            for rec in resp_json['data']:
+
+                # Format the unique database identifier
+                pub_id = 'neot:pub:' + str(rec['PublicationID'])
+
+                # Format author fields
+                author_list = list()
+                if 'Authors' in rec:
+                    for author in rec['Authors']:
+                        author_list.append(author['ContactName'])
+
+                # Look for a DOI in the citation string
+                if 'Citation' in rec:
+                    doi = re.search('(?<=\[DOI:\ ).+(?=\])', rec['Citation'])
+                    if doi:
+                        doi = doi.group()
+                else:
+                    doi = None
+
+                # Build dictionary of bibliographic fields
+                ref = dict()
+                ref.update(kind=rec.get('PubType'),
+                           year=rec.get('Year'),
+                           doi=doi,
+                           authors=author_list,
+                           ident=pub_id,
+                           cite=rec.get('Citation'))
+
+                # Call the appropriate bibliographic formatter
+                if format and format.lower() == 'ris':
+                    bib_obj = format_ris(ref)
+                elif format and format.lower() == 'csv':
+                    bib_obj = format_csv(ref)
+                else:
+                    bib_obj = format_bibjson(ref)
+
+                # Add the fomatted bibliographic reference to the return
+                pub_return.append(bib_obj)
+
+            # Build the JSON description object
+            t1 = round(time.time()-t0, 3)
+            desc_obj.update(neot={'response_time': t1,
+                                  'subqueries': resp.url,
+                                  'record_count': len(resp_json['data'])})
     #
     # Query the Paleobiology Database (Publications)
     #
+
     t0 = time.time()
     payload = dict()
     payload.update(vocab='pbdb', show='both')
@@ -75,12 +140,12 @@ def pub(occid=None, siteid=None, format=None):
     if occid:
         base_url = 'http://paleobiodb.org/data1.2/occs/refs.json'
         payload.update(occ_id=occid)
-    elif siteid:
+    elif locid:
         base_url = 'http://paleobiodb.org/data1.2/colls/refs.json'
-        payload.update(coll_id=siteid)
+        payload.update(coll_id=locid)
     else:
         return jsonify(status_code=400,
-                       error='Specify occurrence ID or site ID.')
+                       error='Specify occurrence ID or locale ID.')
 
     # Issue GET request to database API
     resp = requests.get(base_url, params=payload, timeout=5)
