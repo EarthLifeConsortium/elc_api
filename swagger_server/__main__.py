d8e11d2c664e007195eefed601fb6a92dbc1fd0e
jpjenk <jpjenk@icloud.com>
2017-10-18 09:41:50 -0400

    changed default port for werkzeug to 8008 for hosting

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Composite interface to the Neotoma and PBDB databases'})
-    app.run(port=8080)
+    app.run(port=8008)
