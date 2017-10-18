d3b75e1b1d9f4ce3e567d3a60bf152bd40fa19a6
jpjenk <jpjenk@icloud.com>
2017-10-18 10:14:45 -0400

    changed server loopback to 0.0.0.0

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Composite interface to the Neotoma and PBDB databases'})
-    app.run(port=8008)
+    app.run(host='0.0.0.0', port=8008)
