eeb553daf368a09b376887b4a850c358861509cd
jpjenk <jpjenk@icloud.com>
2018-04-12 09:57:58 -0400

    modified ui elements

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import connexion
-from .encoder import JSONEncoder
+#  from .encoder import JSONEncoder
 
 app = connexion.App(__name__, specification_dir='./swagger/')
-app.app.json_encoder = JSONEncoder
+#  app.app.json_encoder = JSONEncoder
 
 app.add_api('swagger.yaml', arguments={'title': 'Composite interface to the Neotoma and PBDB databases'})
 
