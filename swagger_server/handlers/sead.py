b0abde6822f8cf7a05a8784dbacb74e100c8e41c
jpjenk <jpjenk@icloud.com>
2018-06-05 11:50:20 -0400

    added the sead handler file

---
+++ swagger_server/handlers/sead.py
@@ -0,0 +1,28 @@
+"""Response decoder: The Strategic Environmental Archaeology Database."""
+
+
+def occurrences(resp_json, return_obj, options):
+    """Taxa in time and space."""
+    from ..elc import ages
+
+    #  factor = ages.set_age_scaler(options=options, db='pbdb')
+
+    for rec in resp_json:
+
+        data = dict()
+
+        data.update(occ_id='sead:loc:{0:d}'.format(rec.get('locale_id')))
+        data.update(taxon_id='sead:txn:{0:d}'.format(rec.get('taxon_id')))
+        data.update(taxon=rec.get('taxon'))
+        data.update(source=rec.get('source'))
+        data.update(max_age=None)
+        data.update(min_age=None)
+        data.update(lat=rec.get('lat'))
+        data.update(lon=rec.get('lon'))
+        data.update(data_type=None)
+        data.update(elevation=None)
+        data.update(locale_id=rec.get('locale_id'))
+
+        return_obj.append(data)
+
+    return return_obj
